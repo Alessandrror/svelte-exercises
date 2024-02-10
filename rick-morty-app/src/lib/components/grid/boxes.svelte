714d44b4d01354b850348d9ab627024e335a351d
Alessandrror <ssandrorivas@gmail.com>
2024-02-09 23:52:49 -0600

    :heavy_plus_sign: Adding: new logif ro thodr nyn

--- rick-morty-app/src/lib/components/grid/boxes.svelte
+++ rick-morty-app/src/lib/components/grid/boxes.svelte
@@ -3,11 +3,8 @@
   import Box from "./box.svelte"
   import { getChars } from "./boxes"
 
-  let gridSectionClass = "grid grid-cols sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 place-items-center p-10 m-10 max-w-7xl"
-
 </script>
-
-<section class={ gridSectionClass }>
+<section class="grid grid-cols sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 place-items-center p-10 m-10 max-w-7xl">
   {#await getChars() then { results } }
       {#each results as char}
         <Box char={ char } />
