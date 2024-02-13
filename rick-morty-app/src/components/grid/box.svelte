44c7f782ed589daa018ab10e709847ba3517d7d1
Alessandrror <ssandrorivas@gmail.com>
2024-02-12 22:49:36 -0600

    :art: Updating: character boxes logic

--- rick-morty-app/src/components/grid/box.svelte
+++ rick-morty-app/src/components/grid/box.svelte
@@ -2,22 +2,30 @@
 
 	import { z } from "zod"
   import { ResultCharacterSchema } from "$lib/validation/character"
-  import { ResultLocationSchema } from "$lib/validation/location"
-  import { ResultEpisodeSchema } from "$lib/validation/episode"
+  // import { ResultLocationSchema } from "$lib/validation/location"
+  // import { ResultEpisodeSchema } from "$lib/validation/episode"
 
-  // export let selection: Characters | Locations | Episodes
   // type CharactersResult = z.infer<typeof ResultCharacterSchema>
-  // export let selection: CharactersResult
+  // type LocationResult = z.infer<typeof ResultLocationSchema>
+  // type EpisodeResult = z.infer<typeof ResultEpisodeSchema>
+
+  // export let selection: z.infer<typeof ResultCharacterSchema | typeof ResultLocationSchema | typeof ResultEpisodeSchema>
   export let selection: z.infer<typeof ResultCharacterSchema>
   export let key: string
+
+  const isCharacter = typeof selection === typeof ResultCharacterSchema
+  // const isLocation = typeof selection === typeof ResultLocationSchema
+  // const isEpisode = typeof selection === typeof ResultEpisodeSchema
 
 </script>
 
 <slot>
   <div id={ key } class="p-2 text-center bg-slate-700 rounded-xl">
     {#if selection }
-      <img src={ selection.image } alt={ `${selection.name} Picture` } class="rounded-xl" >
-      <span class="text-2xl text-balance">{ selection.name }</span>
+      {#if isCharacter}
+        <img src={ selection.image } alt={ `${selection.name} Picture` } class="rounded-xl" >
+        <span class="text-2xl text-balance">{ selection.name }</span>
+      {/if}
     {/if}
   </div>
 </slot>
